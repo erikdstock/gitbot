cf59b8d8f7ea0a51184711c9ef515fabc36cd21e
Erik Stockmeier <erik.d.stock@gmail.com>
2017-11-07 14:32:54 -0500

    woot

--- scripts/0-import-es6.js
+++ scripts/0-import-es6.js
@@ -1,6 +1,4 @@
 require("babel-register")
-if (process.env.NPM_ENV === 'production') {
-  require("dotenv").config()
-}
+require("dotenv").config()
 // keep hubot from complaining
 module.exports = function(bot){}
